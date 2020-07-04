5334381d48393f9392a44fa8d690751c72ce6c36
Alexander Konovalov <shuryak@protonmail.com>
2020-07-04 16:15:21 +0300

    feat: added ability to pass props to ArticleThumbnail

--- src/Components/ArticleThumbnail.tsx
+++ src/Components/ArticleThumbnail.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../scss/article-thumbnails.scss';
 
-export const ArticleThumbnail: React.FunctionComponent = () => {
+type ArticleProps = {
+  title: string;
+  image: string;
+}
+
+export const ArticleThumbnail: React.FunctionComponent<ArticleProps> = ({title, image}) => {
   return (
     <div className="article-thumbnail">
-      <h1 className="article-title">Article Title</h1>
+      <h1 className="article-title">{title}</h1>
       <div className="article-fade"/>
-      <img className="article-image" src="" alt="thumbnail"/>
+      <img className="article-image" src={image} alt={title}/>
     </div>
   );
 }
